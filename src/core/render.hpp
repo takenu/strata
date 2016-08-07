a65463bd72f6a3065f6dfb6a9eb8c280ae07c429
takenu <takenu@gmail.com>
2016-08-07 13:36:11 +0200

    Clean up RenderManager; split screen/worldRenderable
    In this commit I split the renderable counter that assigns unique keys
    to Renderable's into two counters: one for the worldRenderables and one
    for the screenRenderables. We will also use only one key map for the
    Renderables.
    
    The general reason for doing things about the RenderManager is that I
    want to have console-like functionality in Strata and for that I want to
    have a better functioning window system. Technically this
    adjusting-the-counter wouldn't be necessary yet, but it would have to be
    done at some point anyways, and I do need to make the RenderManager more
    complex. So I decided to start by improving the things that are already
    there, before adding new stuff.
    
    A long time ago I forked the tiny-game-engine and restructured its
    method of rendering Renderables, which in principle made it possible to
    render things in a different order than they had been added. Now seemed
    like a good moment to actually start implementing functionality that
    allow windows to be in front of each other, without having to completely
    delete and re-add all objects that need to be in front of something
    else.
    
    Consider the following: there is an active window in front of the world,
    and at some moment something happens that requires a new screen element
    to be added that has an effect on something far away (e.g. a new light
    source). Then we would have to first remove all the windows, add the
    light source, and then re-add all the windows. That would be fairly
    ridiculous.
    
    However, now we could in principle allocate 'blocks' of keys to certain
    layers of the UI. We could - for instance - reserve the first 1000 or so
    keys for all the most-faraway things, then the next 1000 keys for
    non-active windows, then 100 keys for the top window and its attributes,
    and then another 1000 keys for drawing the primary UI elements on top of
    everything else (e.g. selected parts of the HUD such as the primary menu
    toolbar).
    
    The actual block reservation system has yet to be developed and I'm not
    even sure if that is what I will do next. After all I don't think it's
    necessary for the foreseeable future - but one day it will be.

--- src/core/render.hpp
+++ src/core/render.hpp
@@ -17,6 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 
+/** Start of counting for default (=unordered) world renderables. */
+#define WORLD_RENDERABLE_DEFAULT_COUNTER_START 100000
+/** Start of counting for default (=unordered) screen renderables.
+  * Such 'unordered' renderables will always be rendered in order of addition to the Renderer.
+  */
+#define SCREEN_RENDERABLE_DEFAULT_COUNTER_START 200000
+
 #include <tiny/draw/worldrenderer.h>
 #include <tiny/draw/renderer.h>
 
@@ -27,6 +34,17 @@ namespace strata
 {
 	namespace core
 	{
+		/** The RenderManager is Strata's final interface towards the rendering capabilities
+		  * of the tiny-game-engine. Its core is the WorldRenderer, which renders to screen
+		  * all the Renderable's added to it. Strata makes explicit use of the WorldRenderer's
+		  * optional resource management, by keeping unique keys to all Renderables.
+		  *
+		  * The Renderer distinguishes two types of renderable objects. Firstly, there are
+		  * WorldRenderables, who have a three-dimensional, spatial representation in the world.
+		  * Secondly, there are ScreenRenderables, who do not have such a representation and are
+		  * merely painted on top of the scene. Examples are UI elements such as menus and text,
+		  * which are printed at screen-constant positions, and lighting effects such as light
+		  * sources. */
 		class RenderManager : public intf::RenderInterface
 		{
 			private:
@@ -34,8 +52,10 @@ namespace strata
 				tiny::vec3 cameraPosition;
 				tiny::vec4 cameraOrientation;
 				bool lodFollowsCamera;
-				unsigned int renderableKeyCounter;
-				std::map<tiny::draw::Renderable *, unsigned int> renderableKeyMap;
+				unsigned int worldRenderableKeyCounter;
+				unsigned int screenRenderableKeyCounter;
+				std::map<tiny::draw::Renderable *, unsigned int> worldRenderableKeyMap;
+				std::map<tiny::draw::Renderable *, unsigned int> screenRenderableKeyMap;
 
 				tiny::draw::WorldRenderer * worldRenderer;
 
@@ -54,68 +74,61 @@ namespace strata
 					worldRenderer->render();
 				}
 
-/*				bool getWorldRenderable(RenderableObjectSpecs & ro)
-				{
-					if(worldRenderables.size() > 0)
-					{
-						ro = worldRenderables.front();
-						worldRenderables.pop_front();
-						return true;
-					}
-					else return false;
-				}
-
-				bool getScreenRenderable(RenderableObjectSpecs & ro)
-				{
-					if(screenRenderables.size() > 0)
-					{
-						ro = screenRenderables.front();
-						screenRenderables.pop_front();
-						return true;
-					}
-					else return false;
-				}*/
-
-				virtual void addWorldRenderable(tiny::draw::Renderable * renderable, const bool & readDepthTex = true, const bool & writeDepthTex = true,
+				virtual void addWorldRenderable(tiny::draw::Renderable * renderable,
+						const bool & readDepthTex = true, const bool & writeDepthTex = true,
 						const tiny::draw::BlendMode & blendMode = tiny::draw::BlendReplace)
 				{
-					if(renderableKeyMap.find(renderable) != renderableKeyMap.end())
+					if(worldRenderableKeyMap.find(renderable) != worldRenderableKeyMap.end())
 					{
-						std::cout << " RenderManager::addWorldRenderable() : ERROR: Renderable "<<renderable<<" with key "<<renderableKeyMap.at(renderable)<<" already exists! Cannot add! "<<std::endl;
+						std::cout << " RenderManager::addWorldRenderable() : ERROR: Renderable "
+							<<renderable<<" with key "<<worldRenderableKeyMap.at(renderable)
+							<<" already exists! Cannot add! "<<std::endl;
 						return;
 					}
-					renderableKeyMap.insert( std::make_pair(renderable, ++renderableKeyCounter) );
-					worldRenderer->addWorldRenderable(renderableKeyCounter, renderable, readDepthTex, writeDepthTex, blendMode);
+					worldRenderableKeyMap.insert( std::make_pair(renderable, ++worldRenderableKeyCounter) );
+					worldRenderer->addWorldRenderable(worldRenderableKeyCounter, renderable,
+							readDepthTex, writeDepthTex, blendMode);
 				}
 
-				virtual void addScreenRenderable(tiny::draw::Renderable * renderable, const bool & readDepthTex = true, const bool & writeDepthTex = true,
+				virtual void addScreenRenderable(tiny::draw::Renderable * renderable,
+						const bool & readDepthTex = true, const bool & writeDepthTex = true,
 						const tiny::draw::BlendMode & blendMode = tiny::draw::BlendReplace)
 				{
-					if(renderableKeyMap.find(renderable) != renderableKeyMap.end())
+					if(screenRenderableKeyMap.find(renderable) != screenRenderableKeyMap.end())
 					{
-						std::cout << " RenderManager::addScreenRenderable() : ERROR: Renderable "<<renderable<<" with key "<<renderableKeyMap.at(renderable)<<" already exists! Cannot add! "<<std::endl;
+						std::cout << " RenderManager::addScreenRenderable() : ERROR: Renderable "
+							<<renderable<<" with key "<<screenRenderableKeyMap.at(renderable)
+							<<" already exists! Cannot add! "<<std::endl;
 						return;
 					}
-					renderableKeyMap.insert( std::make_pair(renderable, ++renderableKeyCounter) );
-					worldRenderer->addScreenRenderable(renderableKeyCounter, renderable, readDepthTex, writeDepthTex, blendMode);
+					screenRenderableKeyMap.insert( std::make_pair(renderable, ++screenRenderableKeyCounter) );
+					worldRenderer->addScreenRenderable(screenRenderableKeyCounter, renderable,
+							readDepthTex, writeDepthTex, blendMode);
 				}
 
-				virtual void freeWorldRenderable(tiny::draw::Renderable * renderable) { worldRenderer->freeWorldRenderable(renderableKeyMap.find(renderable)->second); renderableKeyMap.erase(renderable); }
-				virtual void freeScreenRenderable(tiny::draw::Renderable * renderable) { worldRenderer->freeScreenRenderable(renderableKeyMap.find(renderable)->second); renderableKeyMap.erase(renderable); }
-
-				// The list-first, add-to-world-renderer-later doesn't seem very necessary - we could skip the RenderableObjectSpecs altogether and
-				// redirect addWorldRenderable() to worldRenderer->addWorldRenderable(). Commented out for now, likely to be deleted later.
-/*				void getRenderables(void)
+				virtual void freeWorldRenderable(tiny::draw::Renderable * renderable)
 				{
-					RenderableObjectSpecs ro(0,true,true,tiny::draw::BlendReplace);
-					while(getWorldRenderable(ro)) worldRenderer->addWorldRenderable(
-				}*/
+					worldRenderer->freeWorldRenderable(worldRenderableKeyMap.find(renderable)->second);
+					worldRenderableKeyMap.erase(renderable);
+				}
+
+				virtual void freeScreenRenderable(tiny::draw::Renderable * renderable)
+				{
+					worldRenderer->freeScreenRenderable(screenRenderableKeyMap.find(renderable)->second);
+					screenRenderableKeyMap.erase(renderable);
+				}
+
 			public:
 				RenderManager(intf::ApplInterface * _interface) :
 					intf::RenderInterface(),
 					applInterface(_interface),
-					cameraPosition(tiny::vec3(0.001f, 20.0f, 3.001f)), cameraOrientation(tiny::vec4(0.0f, 0.0f, 0.0f, 1.0f)), lodFollowsCamera(true), renderableKeyCounter(0),
-					worldRenderer(new tiny::draw::WorldRenderer(applInterface->getScreenWidth(), applInterface->getScreenHeight()))
+					cameraPosition(tiny::vec3(0.001f, 20.0f, 3.001f)),
+					cameraOrientation(tiny::vec4(0.0f, 0.0f, 0.0f, 1.0f)),
+					lodFollowsCamera(true),
+					worldRenderableKeyCounter(WORLD_RENDERABLE_DEFAULT_COUNTER_START),
+					screenRenderableKeyCounter(SCREEN_RENDERABLE_DEFAULT_COUNTER_START),
+					worldRenderer(new tiny::draw::WorldRenderer(
+								applInterface->getScreenWidth(), applInterface->getScreenHeight()))
 				{
 				}
 				
@@ -127,8 +140,18 @@ namespace strata
 				virtual bool lodUpdates(void) const { return lodFollowsCamera; }
 				virtual tiny::vec3 getCameraPosition(void) const { return cameraPosition; }
 				virtual tiny::vec4 getCameraOrientation(void) const { return cameraOrientation; }
-				void setCameraPosition(tiny::vec3 pos) { cameraPosition = pos; worldRenderer->setCamera(cameraPosition, cameraOrientation); }
-				void setCameraOrientation(tiny::vec4 orient) { cameraOrientation = orient; worldRenderer->setCamera(cameraPosition, cameraOrientation); }
+				void setCameraPosition(tiny::vec3 pos)
+				{
+					cameraPosition = pos;
+					worldRenderer->setCamera(cameraPosition, cameraOrientation);
+				}
+
+				void setCameraOrientation(tiny::vec4 orient)
+				{
+					cameraOrientation = orient;
+					worldRenderer->setCamera(cameraPosition, cameraOrientation);
+				}
+
 				void setLodFollowsCamera(bool b) { lodFollowsCamera = b; }
 		};
 	}
